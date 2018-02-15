5b64cc11e07f63fa35bcd59592480fa6991b112f
KadoTomohiro <kado.tomohiro@jp.fujitsu.com>
2018-02-15 15:31:47 +0900

    create project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bct-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bct';
+}
